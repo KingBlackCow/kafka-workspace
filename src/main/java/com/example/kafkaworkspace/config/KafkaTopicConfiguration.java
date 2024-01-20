38ff7c3101e911aab65a1fe87d759045f37c049a
KingBlackCow <sgs1159@naver.com>
2024-01-20 22:35:25 +0900

    feat: kafka topic list, description and delete

--- src/main/java/com/example/kafkaworkspace/config/KafkaTopicConfiguration.java
+++ src/main/java/com/example/kafkaworkspace/config/KafkaTopicConfiguration.java
@@ -2,14 +2,42 @@ package com.example.kafkaworkspace.config;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
 import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
+import java.util.Collections;
+import java.util.Map;
+
 @Configuration
 public class KafkaTopicConfiguration {
+
+    // 토픽리스트 확인
+    @Bean
+    public ApplicationRunner runner(AdminClient adminClient) {
+        return args -> {
+            Map<String, TopicListing> topics = adminClient.listTopics().namesToListings().get();
+            for (String topicName : topics.keySet()) {
+                // 토픽 리스트 확인
+                TopicListing topicListing = topics.get(topicName);
+                System.out.println(topicListing);
+
+                // 토픽 상세 설명
+                Map<String, TopicDescription> description = adminClient.describeTopics(Collections.singleton(topicName)).all().get();
+                System.out.println(description);
+
+                // 토픽삭제
+                if (!topicListing.isInternal()) {
+                    adminClient.deleteTopics(Collections.singleton(topicName));
+                }
+            }
+        };
+    }
 
     @Bean
     public AdminClient adminClient(KafkaAdmin kafkaAdmin) {
