d8d13098c3929351f90b0f2523e4720683e4ee60
KingBlackCow <sgs1159@naver.com>
2024-01-23 01:18:58 +0900

    feat: KafkaListener 메타정보 가져오기

--- src/main/java/com/example/kafkaworkspace/KafkaWorkspaceApplication.java
+++ src/main/java/com/example/kafkaworkspace/KafkaWorkspaceApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
-
-import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class KafkaWorkspaceApplication {
@@ -19,15 +16,7 @@ public class KafkaWorkspaceApplication {
     @Bean
     public ApplicationRunner runner(Producer producer) {
         return args -> {
-//            kafkaTemplate.send("producer_topic", "hello, hi");
-//            producer.async("producer_topic", "hello, async");
-//            producer.sync("producer_topic", "hello, sync");
-//            producer.routingSend("producer_topic", "Hello, routing");
-//            producer.routingSendBytes("producer_route_topic", "Hello, Bytes".getBytes(StandardCharsets.UTF_8));
-//            producer.replyingSend("request", "Ping");
-
-
-            Thread.sleep(1000L);
+            producer.async("consumer-listener", "Hello,  Container");
         };
     }
 }
--- src/main/java/com/example/kafkaworkspace/consumer/Consumer.java
+++ src/main/java/com/example/kafkaworkspace/consumer/Consumer.java
@@ -1,26 +1,45 @@
 package com.example.kafkaworkspace.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
+import org.springframework.messaging.handler.annotation.Header;
+
+import java.util.Date;
+
 
 @Service
 public class Consumer {
 
-    @KafkaListener(id = "producer-topic-id", topics = "producer_topic")
-    public void listenClip3(String message) {
-        System.out.println(message);
-    }
+//    @KafkaListener(id = "producer-topic-id", topics = "producer_topic")
+//    public void listenClip3(String message) {
+//        System.out.println(message);
+//    }
+//
+//    @KafkaListener(id = "producer-route_topic-bytes-id", topics = "producer_route_topic")
+//    public void listenClip3Bytes(String message) {
+//        System.out.println(message);
+//    }
+//
+//    @KafkaListener(id = "producer-request-id", topics = "request")
+//    @SendTo
+//    public String listenClipRequest(String message) {
+//        System.out.println(message);
+//        return "Pong";
+//    }
 
-    @KafkaListener(id = "producer-route_topic-bytes-id", topics = "producer_route_topic")
-    public void listenClip3Bytes(String message) {
-        System.out.println(message);
-    }
-
-    @KafkaListener(id = "producer-request-id", topics = "request")
-    @SendTo
-    public String listenClipRequest(String message) {
-        System.out.println(message);
-        return "Pong";
+    @KafkaListener(id = "clip4-listener-id", topics = "consumer-listener")
+    public void listen(String message,
+                       @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp,
+                       @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+                       @Header(KafkaHeaders.OFFSET) long offset,
+                       ConsumerRecordMetadata metadata) {
+        System.out.println("Listener. offset=" + metadata.offset() +
+                ", partition=" + partition +
+                ", timestamp=" + new Date(timestamp) +
+                ", message=" + message
+        );
     }
 }
